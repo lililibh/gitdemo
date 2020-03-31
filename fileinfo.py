a87ead8571e4d27543eb69a316ca8cfb55f7e208
lbh <1021285622@qq.com>
2020-03-31 14:24:41 +0800

    update by the seconf developer

--- fileinfo.py
+++ fileinfo.py
@@ -86,3 +86,89 @@ def titinsert():         #插入题目
     tit.mainloop()
 
 #审阅模块
+Num=[]
+def sum_fraquency():        #词频统计
+    worddict={}
+    for j in Wordlist[0]:
+        if j in worddict:
+            worddict[j]+=1
+        else:
+            worddict[j]=1
+    Num.append(len(worddict))
+    return worddict
+def num():
+    ab=sum_fraquency()
+    print('总词数为'+str(Num[0]))
+def rank():                  #关键词统计
+    useword={}
+    worddict=sum_fraquency()
+    for key in worddict:
+        if key in Liststop[0]:
+            continue
+        else:
+            useword[key]=worddict[key]
+    orderword=sorted(useword.items(),key=lambda d:d[1],reverse=True)
+    print('关键词为：',orderword[0:6])
+    return orderword[0:6]
+
+def draft():                 #绘制词频图
+    orderword=rank()
+    word=[]
+    fluency=[]
+    for i in orderword:
+        word.append(i[0])
+        fluency.append(i[1])
+    plt.figure(figsize=(9,6))
+    idx = np.arange(6)
+    width=0.5
+    plt.bar(idx,fluency,width, color='red')
+    plt.xlabel('单词')
+    plt.ylabel('词频')
+    plt.xticks(idx+width/2,word)
+    plt.show()
+
+Find=[]
+def finds():                     #查找功能
+    def finding():
+        Find.append(a.get())
+      #  print(Find)
+        if Find[0] in Article[0]:
+            weizhi=Article[0].index(Find[0])
+            hang=weizhi//45
+            lie=weizhi%45
+            print('在第'+str(hang+1)+'行,第'+str(lie)+'列')
+        else:
+            print('没有找到！')
+        find.destroy()
+    find=tkinter.Tk()
+    find.title('查找')
+    tkinter.Label(find,text='请输入要查找的内容').grid(row = 0, column = 0)
+    a=tkinter.Entry(find)
+    a.grid(row = 0, column = 1)
+    tkinter.Button(find,text='确认查找',command=finding).grid(row=0,column=2)
+    find.mainloop()
+    
+#查看帮助模块
+def helpyou():  
+    webbrowser.open('大作业答辩.pptx')
+top=tkinter.Tk()
+frame=tkinter.Frame(height=20,width=20)
+frame.pack() 
+textbox=tkinter.Text(top,width=45,height=15,font=48)
+textbox.pack()
+label1=tkinter.Label(frame,text='开始',fg='blue',font=("黑体", 10,"normal")).grid(row=0,column=0)
+label2=tkinter.Label(frame,text='插入',fg='blue',font=("黑体", 10,"normal")).grid(row=1,column=0)
+label3=tkinter.Label(frame,text='审阅',fg='blue',font=("黑体", 10,"normal")).grid(row=2,column=0)
+label4=tkinter.Label(frame,text='查找',fg='blue',font=("黑体", 10,"normal")).grid(row=3,column=0)
+button1=tkinter.Button(frame,text=' 查看关键词 ',command=rank,font=("微软雅黑", 9,"normal")).grid(row = 2, column = 3)
+button2=tkinter.Button(frame,text='插入当前日期',command=timeinsert,font=("微软雅黑", 9,"normal")).grid(row = 1, column = 3)
+button3=tkinter.Button(frame,text=' 绘制词频图',command=draft,font=("微软雅黑", 9,"normal")).grid(row = 2, column = 4)
+button4=tkinter.Button(frame,text='  查看帮助  ',command=helpyou,font=("微软雅黑", 9,"normal")).grid(row = 0, column = 4)
+button5=tkinter.Button(frame,text='    退出    ',command=close,font=("微软雅黑", 9,"normal")).grid(row = 0, column = 5)
+button6=tkinter.Button(frame,text='    查找    ',command=finds,font=("微软雅黑", 9,"normal")).grid(row = 3, column = 3)
+button7=tkinter.Button(frame,text='  插入作者  ',command=authorinsert,font=("微软雅黑", 9,"normal")).grid(row = 1, column =4)
+button8=tkinter.Button(frame,text='  插入题目  ',command=titinsert,font=("微软雅黑", 9,"normal")).grid(row = 1, column =5)
+button9=tkinter.Button(frame,text=' 查看总词数 ',command=num,font=("微软雅黑", 9,"normal")).grid(row = 2, column =5 )
+button10=tkinter.Button(frame,text='    打开     ',command=turnon,font=("微软雅黑", 9,"normal")).grid(row = 0, column = 3)
+
+top.mainloop()
